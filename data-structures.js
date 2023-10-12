293a4e6f2381794bab6c9ef2338c0684c7f4d0e8
jeremiahlouis <131529321+jjboy55@users.noreply.github.com>
2023-10-12 21:03:27 +0100

    Update data-structures.js

---
+++ data-structures.js
@@ -0,0 +1,9 @@
+// ----------------------------------------BIG 0 NOTATION--------------------------------------------//
+//EXAMPLES O(n)
+
+function logAtleast5(n) {
+    for (let index = 0; index < Math.max(5,n); index++) {
+        console.log(index);
+    }
+}
+
